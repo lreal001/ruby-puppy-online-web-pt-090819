9a94dbfc54fb5c30c496200a90890a35906a2c76
Lessllie Real <rlessllie@yahoo.com>
2019-11-11 02:50:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,35 @@
 # Add your code here
+
+class Dog 
+  
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end 
+  
+  @@all = []
+  
+ def self.all
+   @@all
+ end 
+ 
+ def self.print_all
+   puts "#{@@all}"
+ end
+ 
+ def self.clear_all 
+   
+ 
+ end 
+    
+    
+    
+  end 
+  
+  pluto = Dog.new("Pluto")
+  fido = Dog.new("Fido")
+  maddy = Dog.new("Maddy")
+  
+  
